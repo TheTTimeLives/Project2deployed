1869ed70bbc6902bf8476db7d7376a836d6b3bd3
Taylor M <melnikprod@gmail.com>
2018-10-02 10:05:56 -0400

    Fixed login button, login appearance in main page and database clearing

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require('./controller/routes/apiroutes.js')(app);
 
 
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function (){
         console.log(`App listening on PORT: ${PORT}`);
     })
